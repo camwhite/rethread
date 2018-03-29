852e4c68c681b408c07ecf739c68b40a360301a3
Cameron White <camw85@mac.com>
2018-03-29 16:36:23 -0600

    :sunrise: initial commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'rethread.io'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
